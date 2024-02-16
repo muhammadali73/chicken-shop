3ea25012a9ff5657ca3d0406300beb6bb7f1ce5d
muhammad ali <muhammadali30302@gmail.com>
2024-02-16 16:10:38 +0500

    chicken price calculate

---
+++ main.js
@@ -0,0 +1,9 @@
+// a = (prompt("Enter Your First Name"))
+// b = (prompt("Enter Your Last Name"))
+
+// document.write(`<h1>`,a + "  " + b,`</h1>`)
+a = parseInt(prompt("how much chicken is required in grams (1kg = RS: 500"))
+
+document.write("You need to pay Rs: ",a/2)
+
+// document.write("You need to pay Rs: ",a*500/1000)
